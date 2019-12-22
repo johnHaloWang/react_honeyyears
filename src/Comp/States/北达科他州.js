a860e7dd1b95f9e879467b71174443b98c5448ec
John Wang <johnwang@Johns-MacBook-Pro.local>
2019-12-21 20:36:01 -0800

    fifth commit cssStyling1 video

--- src/Comp/States/北达科他州.js
+++ src/Comp/States/北达科他州.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import '../../App.css';
+import YouTube from 'react-youtube';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function 北达科他州() {
   return (
@@ -13,15 +17,91 @@ function 北达科他州() {
                 <h2>北达科他州</h2>
             </header>
             <br></br>
-            <p class="jumbotron">
-            <center>Contens coming soon</center><br></br>
-            </p>
-            <ul class="actions">
-                <li><a href="#" class="button alt">...</a></li>
-            </ul>
+            <div class="jumbotron">
+                <p >
+                    <center>Contents coming soon</center><br></br>
+                </p>
+                <br></br>
+                <div class="card-deck mb-3 text-center">
+                    <div class="card mb-4 shadow-sm">
+                        <YouTube
+                            videoId="4x0_e0t7tUo"
+                            opts={{
+                            height: '200',
+                            width: '250',
+                            playerVars: {
+                                autoplay: 1
+                            }}
+                            }
+                            onReady={_onReady}
+                        />
+                    </div>
+                    <div class="card mb-4 shadow-sm">
+                        <YouTube
+                            videoId="4x0_e0t7tUo"
+                            opts={{
+                            height: '200',
+                            width: '250',
+                            playerVars: {
+                                autoplay: 1
+                            }}
+                            }
+                            onReady={_onReady}
+                        />
+                    </div><div class="card mb-4 shadow-sm">
+                        <YouTube
+                            videoId="4x0_e0t7tUo"
+                            opts={{
+                            height: '200',
+                            width: '250',
+                            playerVars: {
+                                autoplay: 1
+                            }}
+                            }
+                            onReady={_onReady}
+                        />
+                    </div><div class="card mb-4 shadow-sm">
+                        <YouTube
+                            videoId="4x0_e0t7tUo"
+                            opts={{
+                            height: '200',
+                            width: '250',
+                            playerVars: {
+                                autoplay: 1
+                            }}
+                            }
+                            onReady={_onReady}
+                        />
+                    </div>
+                    
+                </div>
+                {/* <YouTube
+                    videoId="4x0_e0t7tUo"
+                    opts={{
+                    height: '40',
+                    width: '60',
+                    playerVars: {
+                        autoplay: 1
+                    }}
+                    }
+                    onReady={_onReady}
+                /> */}
+                <br></br>
+
+                <ul class="actions">
+                    <li><a href="#" class="button alt">button</a></li>
+                </ul>
+            </div>
+            
         </div>
     </section>
   );
 }
 
+function _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  };
+  
+  
 export default 北达科他州;
